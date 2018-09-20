1ea74173cc461c6534492d676338cdddf5659e79
Oliver De La Via <odelavia@gmail.com>
2018-09-20 08:56:15 -0400

    initial commit

---
+++ client/android/app/src/main/java/com/foodbears/MainApplication.java
@@ -0,0 +1,59 @@
+package com.foodbears;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.christopherdro.RNPrint.RNPrintPackage;
+import com.wix.interactable.Interactable;
+import com.imagepicker.ImagePickerPackage;
+import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new SvgPackage(),
+            new SplashScreenReactPackage(),
+            new RNPrintPackage(),
+            new Interactable(),
+            new ImagePickerPackage(),
+            new RNHTMLtoPDFPackage(),
+            new RNCameraPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
