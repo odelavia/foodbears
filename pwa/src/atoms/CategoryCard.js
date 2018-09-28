ae57d016870f02d03fb3a3eb3859e46edee2c057
Oliver De La Via <odelavia@gmail.com>
2018-09-27 22:09:42 -0400

    added onClick method to categories in prep for routing

--- pwa/src/atoms/CategoryCard.js
+++ pwa/src/atoms/CategoryCard.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CategoryCard = ({item, category}) => {
-  return (
-    <div>
-      {category}
-    </div>
-  );
+class CategoryCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(val) {
+    return console.log("you clicked the ", val, " category.")
+  }
+
+  render() {
+    const { category, item } = this.props;
+    return (
+      <div onClick={() => this.onClick(category)}>
+        {category}
+      </div>
+    );
+  }
+
 }
 
 export default CategoryCard;
