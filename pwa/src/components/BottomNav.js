a2864e537e8923d78923f5d78fbd2531f67c44dd
Oliver De La Via <odelavia@gmail.com>
2018-10-26 00:52:00 -0400

    added links and styling to bottom nav

--- pwa/src/components/BottomNav.js
+++ pwa/src/components/BottomNav.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class BottomNav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      bottom: -250,
+    };
+
+    this.openNav = this.openNav.bind(this);
+  }
+
+  openNav() {
+    this.setState(currentState => {
+      return {
+        open: !currentState.open,
+        bottom: currentState.bottom === -250 ? 0 : -250,
+      }
+    },
+    () => console.log(`nav is ${this.state.open}`))
+  }
+
   render() {
     return (
-      <div>
-        BottomNav
+      <div className="dragnav-wrapper" style={{bottom: `${this.state.bottom}px`}}>
+        <div className="dragnav-container">
+          <button className="js-dragnav-toggle" onClick={this.openNav} draggable onDrag={this.openNav} type="button" aria-label="Toggle navigation">
+            <span className="dragnav-toggle__icon" />
+          </button>
+        </div>
+        <div className="outsideAlert">
+          <ul className="l-dragnav-list">
+            <NavLink className="dragnav-list__item-1" to="/" onClick={this.openNav}>Login</NavLink>
+            <NavLink className="dragnav-list__item-1" to="/" onClick={this.openNav}>Categories</NavLink>
+            <NavLink className="dragnav-list__item-2" to="/" onClick={this.openNav}>Cart</NavLink>
+          </ul>
+        </div>
       </div>
     );
   }
