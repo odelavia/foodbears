aa58de5a35947a85b45edbe7b668bb54c143d43d
Oliver De La Via <odelavia@gmail.com>
2018-10-25 22:10:06 -0400

    initial navbar

--- pwa/src/components/Nav.js
+++ pwa/src/components/Nav.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Nav extends Component {
   render() {
     return (
-      <div>
-        nav
+      <div className="nav-wrapper">
+        <div className="nav-container">
+          
+        </div>
       </div>
     );
   }
